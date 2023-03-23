ff36fc1c72b905afa974514944de9c35468b5eaf
carlos8657 <carlos865@outlook.com>
2023-03-23 11:52:09 -0700

    Barra de busqueda

--- models/funciones.js
+++ models/funciones.js
@@ -3,11 +3,11 @@ const conexion = require('../models/db');
 exports.save = (req,res)=>{
     const descripcion = req.body.descripcion;
     const precio = req.body.precio;
-    const stats = req.body.stats;
     const imagen = req.body.imagen;
     const stock = req.body.stock;
     const categoria = req.body.categoria;
-    conexion.query('Insert into productos set ?', {descripcion: descripcion, precio: precio, stats: stats, imagen: imagen, stock: stock, categoria: categoria}, (error,results)=>{
+    const estado = req.body.estado;
+    conexion.query('Insert into productos set ?', {descripcion: descripcion, precio: precio, imagen: imagen, stock: stock, categoria: categoria, estado : 1}, (error,results)=>{
         if(error){
             console.log(error);
         }else{
@@ -17,19 +17,32 @@ exports.save = (req,res)=>{
 }
 
 exports.update = (req,res)=>{
-    const idProducto = req.params.id;
+    const {id} = req.params;
     const descripcion = req.body.descripcion;
     const precio = req.body.precio;
-    const stats = req.body.stats;
     const imagen = req.body.imagen;
     const stock = req.body.stock;
     const categoria = req.body.categoria;
-    conexion.query('Update productos set ? where idProducto = ?',[{descripcion: descripcion, precio: precio, stats: stats, imagen: imagen, stock: stock, categoria: categoria},idProducto],(error,results)=>{
+    const estado = req.body.estado;
+    conexion.query('Update productos set ? where idProducto = ?',[{descripcion: descripcion, precio: precio, imagen: imagen, stock: stock, categoria: categoria, estado: estado},id],(error,results)=>{
         if(error){
             console.log(error);
         }else{
             res.redirect('/administrador');
         }
     })
+}
+
+exports.buscar = (req,res)=>{
+    const palabra = req.body.palabra;
+    conexion.query("select * from productos where idProducto like '%"+ palabra +"%' or descripcion like '%"+ palabra +"%' or precio like '%"+ palabra +"%' or imagen like '%"+ palabra +"%' or stock like '%"+ palabra +"%' or categoria like '%"+ palabra +"%' or estado like '%"+ palabra +"%'",(error,results,fields)=>{
+        if(error){
+            console.log(error);
+        }else{
+            fields[fields.length] = {name: 'Acciones'}
+            res.render('busqueda.html',{results,fields});
+        }
+    })
+
 
 }
