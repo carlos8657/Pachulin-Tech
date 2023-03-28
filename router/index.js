e0fdc7a258d7de5d0152a49b65058339593adc7d
Omar Osuna <2020030397@upsin.edu.mx>
2023-03-27 22:35:42 -0700

    Cambios de diseño

--- router/index.js
+++ router/index.js
@@ -11,7 +11,7 @@ router.get('/administrador', async (req,res)=>{
             throw error;
         }else{
             fields[fields.length] = {name: 'Acciones'}
-            res.render('administrador.html',{results,fields});
+            res.render('administrador2.html',{results,fields});
         }
     })
     
